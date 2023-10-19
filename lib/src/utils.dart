37a08b7a81494a9d42c137bbc7a9ed568daf2a44
Michele Lacorte <michele.lacorte@quinck.io>
2023-10-19 16:31:31 +0200

    added static dialog (4)

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -4,27 +4,29 @@ import 'package:flutter/material.dart';
 
 import '../country_code_picker.dart';
 
-void showStaticDialog(BuildContext context,
-    Function(CountryCode) onChanged) async {
-  List<Map<String, String>> jsonList = codes;
+class Utils {
+  void showStaticDialog(BuildContext context,
+      Function(CountryCode) onChanged) async {
+    List<Map<String, String>> jsonList = codes;
 
-  List<CountryCode> elements =
-  jsonList.map((json) => CountryCode.fromJson(json)).toList();
+    List<CountryCode> elements =
+    jsonList.map((json) => CountryCode.fromJson(json)).toList();
 
-  final item = await showDialog(
-    barrierColor: Colors.grey.withOpacity(0.5),
-    context: context,
-    builder: (context) =>
-        Center(
-          child: Dialog(
-            child: SelectionDialog(
-              elements,
-              [],
-              showCountryOnly: false,
-              showFlag: true,
+    final item = await showDialog(
+      barrierColor: Colors.grey.withOpacity(0.5),
+      context: context,
+      builder: (context) =>
+          Center(
+            child: Dialog(
+              child: SelectionDialog(
+                elements,
+                [],
+                showCountryOnly: false,
+                showFlag: true,
+              ),
             ),
           ),
-        ),
-  );
-  onChanged.call(item);
+    );
+    onChanged.call(item);
+  }
 }
